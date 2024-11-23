e60950dca8af75a0f8fe55f4f143cb509860ddc8
freetouse1234 <freetousebmsit@gmail.com>
2024-11-23 17:10:23 +0530

    commit

--- Gradelytics/settings.py
+++ Gradelytics/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
     }
 }
 
-database_url=os.environ.get("DATABASE_URL")
-DATABASES['default']=dj_database_url.parse(database_url)
+database_urls=os.environ.get("DATABASE_URL")
+DATABASES['default']=dj_database_url.parse(database_urls)
 
 # DATABASES = {
 #     'default': {
